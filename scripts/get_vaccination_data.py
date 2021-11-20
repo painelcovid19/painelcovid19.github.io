74a46dd7bbf1598ab0371b4918bbeb3dd8291d54
josedembo <josedembo18@aluno.unilab.edu.br>
2021-11-20 05:56:54 -0300

    adiciona  workflow  para gerar o html das macroregioes

--- scripts/get_vaccination_data.py
+++ scripts/get_vaccination_data.py
@@ -16,7 +16,7 @@ client = Elasticsearch(
         "Content-Type": "application/json",
         "Cookie": "ELASTIC-PROD=1618079452.839.9136.791476",
     },
-    request_timeout = 60000
+    request_timeout = 30
 )
 
 for key in locations.keys():
