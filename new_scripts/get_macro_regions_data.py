eef3d15d0bbd1d9afd0d0ad1df43dd6969f4b923
josedembo <josedembo18@aluno.unilab.edu.br>
2022-03-29 19:01:36 -0300

    Remove o pacote dotenv e cria a funcao main

--- new_scripts/get_macro_regions_data.py
+++ new_scripts/get_macro_regions_data.py
@@ -1,134 +1,140 @@
 from urllib import response
-import requests 
+import requests
 from packages.api import url, getYesterdaysDate
 from packages.ibge_codes import codigosIBG_CE, codigosIBG_BA
-from dotenv import load_dotenv, find_dotenv
+import logging
+import sys
 import csv
 import os
 
-load_dotenv(find_dotenv())
+logging.basicConfig(level=logging.DEBUG)
 
-api_key = os.getenv("API_KEY")
-
-headers = {"authorization": f"Token {api_key}"}
 
 date = getYesterdaysDate()
-payload = {
-        "state": "CE",
-        "had_cases":True,
-        "date":"{}".format(date)
-    }
+payload = {"state": "CE", "had_cases": True, "date": "{}".format(date)}
 
-payload_baiha = {
-        "state": "BA",
-        "had_cases":True,
-        "date":"{}".format(date)
-    }
+payload_baiha = {"state": "BA", "had_cases": True, "date": "{}".format(date)}
 
-columns =[
-            "city",
-            "city_ibge_code",
-            "date",
-            "last_available_confirmed",
-            "last_available_confirmed_per_100k_inhabitants",
-            "last_available_deaths_per_100k_inhabitants",
-            "estimated_population_2019",
-            "last_available_deaths",
-            "state",
-            "new_confirmed",
-            "new_deaths",
-        ]
+columns = [
+    "city",
+    "city_ibge_code",
+    "date",
+    "last_available_confirmed",
+    "last_available_confirmed_per_100k_inhabitants",
+    "last_available_deaths_per_100k_inhabitants",
+    "estimated_population_2019",
+    "last_available_deaths",
+    "state",
+    "new_confirmed",
+    "new_deaths",
+]
 
-response = requests.get(url, headers=headers, params=payload)
+directory = "./data"
 
-data = response.json()
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
-data = data["results"]
 
-with open("data/df_dados_macro_regioes_ceara.csv", "w", newline="", encoding="utf-8") as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(columns)
+def main(api_key: str):
 
-    for row in data:
-        for cod in codigosIBG_CE:
-            if row["city_ibge_code"] == cod:
-                city = row["city"]
-                # logging.info(f"Coletando dados de {city}")
-                city_ibge_code = row["city_ibge_code"]
-                date = row["date"]
-                last_available_confirmed = row["last_available_confirmed"]
-                last_available_confirmed_per_100k_inhabitants = row[
+    headers = {"authorization": f"Token {api_key}"}
+
+    response = requests.get(url, headers=headers, params=payload)
+
+    data = response.json()
+
+    data = data["results"]
+
+    with open(
+        f"{directory}/df_dados_macro_regioes_ceara.csv", "w", newline="", encoding="utf-8"
+    ) as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(columns)
+
+        for row in data:
+            for cod in codigosIBG_CE:
+                if row["city_ibge_code"] == cod:
+                    city = row["city"]
+                    logging.info(f"Coletando dados de {city}")
+                    city_ibge_code = row["city_ibge_code"]
+                    date = row["date"]
+                    last_available_confirmed = row["last_available_confirmed"]
+                    last_available_confirmed_per_100k_inhabitants = row[
                         "last_available_confirmed_per_100k_inhabitants"
-                ]
-                estimated_population_2019 = row["estimated_population_2019"]
-                last_available_deaths = row["last_available_deaths"]
-                state = row["state"]
-                new_confirmed = row["new_confirmed"]
-                new_deaths = row["new_deaths"]
-                last_available_deaths_per_100k_inhabitants = round(
-                    (last_available_deaths / estimated_population_2019 * 100000), 4
-                )
-                csv_writer.writerow(
-                    [
-                        city,
-                        city_ibge_code,
-                        date,
-                        last_available_confirmed,
-                        last_available_confirmed_per_100k_inhabitants,
-                        last_available_deaths_per_100k_inhabitants,
-                        estimated_population_2019,
-                        last_available_deaths,
-                        state,
-                        new_confirmed,
-                        new_deaths,
                     ]
-                )
+                    estimated_population_2019 = row["estimated_population_2019"]
+                    last_available_deaths = row["last_available_deaths"]
+                    state = row["state"]
+                    new_confirmed = row["new_confirmed"]
+                    new_deaths = row["new_deaths"]
+                    last_available_deaths_per_100k_inhabitants = round(
+                        (last_available_deaths / estimated_population_2019 * 100000), 4
+                    )
+                    csv_writer.writerow(
+                        [
+                            city,
+                            city_ibge_code,
+                            date,
+                            last_available_confirmed,
+                            last_available_confirmed_per_100k_inhabitants,
+                            last_available_deaths_per_100k_inhabitants,
+                            estimated_population_2019,
+                            last_available_deaths,
+                            state,
+                            new_confirmed,
+                            new_deaths,
+                        ]
+                    )
 
+    # coletando os dados da Baiha
 
-# coletando os dados da Baiha
+    response_baiha = requests.get(url, headers=headers, params=payload_baiha)
 
-response_baiha = requests.get(url, headers=headers, params=payload_baiha)
+    data_baiha = response_baiha.json()
 
-data_baiha = response_baiha.json()
+    data_baiha = data_baiha["results"]
 
-data_baiha = data_baiha["results"]
+    with open(
+        f"{directory}/df_dados_macro_regioes_bahia.csv", "w", newline="", encoding="utf-8"
+    ) as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(columns)
 
-with open("data/df_dados_macro_regioes_bahia.csv", "w", newline="", encoding="utf-8") as csv_file:
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(columns)
-
-    for row in data_baiha:
-        for cod in codigosIBG_BA:
-            if row["city_ibge_code"] == cod:
-                city = row["city"]
-                # logging.info(f"Coletando dados de {city}")
-                city_ibge_code = row["city_ibge_code"]
-                date = row["date"]
-                last_available_confirmed = row["last_available_confirmed"]
-                last_available_confirmed_per_100k_inhabitants = row[
+        for row in data_baiha:
+            for cod in codigosIBG_BA:
+                if row["city_ibge_code"] == cod:
+                    city = row["city"]
+                    logging.info(f"Coletando dados de {city}")
+                    city_ibge_code = row["city_ibge_code"]
+                    date = row["date"]
+                    last_available_confirmed = row["last_available_confirmed"]
+                    last_available_confirmed_per_100k_inhabitants = row[
                         "last_available_confirmed_per_100k_inhabitants"
-                ]
-                estimated_population_2019 = row["estimated_population_2019"]
-                last_available_deaths = row["last_available_deaths"]
-                state = row["state"]
-                new_confirmed = row["new_confirmed"]
-                new_deaths = row["new_deaths"]
-                last_available_deaths_per_100k_inhabitants = round(
-                    (last_available_deaths / estimated_population_2019 * 100000), 4
-                )
-                csv_writer.writerow(
-                    [
-                        city,
-                        city_ibge_code,
-                        date,
-                        last_available_confirmed,
-                        last_available_confirmed_per_100k_inhabitants,
-                        last_available_deaths_per_100k_inhabitants,
-                        estimated_population_2019,
-                        last_available_deaths,
-                        state,
-                        new_confirmed,
-                        new_deaths,
                     ]
-                )
+                    estimated_population_2019 = row["estimated_population_2019"]
+                    last_available_deaths = row["last_available_deaths"]
+                    state = row["state"]
+                    new_confirmed = row["new_confirmed"]
+                    new_deaths = row["new_deaths"]
+                    last_available_deaths_per_100k_inhabitants = round(
+                        (last_available_deaths / estimated_population_2019 * 100000), 4
+                    )
+                    csv_writer.writerow(
+                        [
+                            city,
+                            city_ibge_code,
+                            date,
+                            last_available_confirmed,
+                            last_available_confirmed_per_100k_inhabitants,
+                            last_available_deaths_per_100k_inhabitants,
+                            estimated_population_2019,
+                            last_available_deaths,
+                            state,
+                            new_confirmed,
+                            new_deaths,
+                        ]
+                    )
 
+
+if __name__ == "__main__":
+    main(sys.argv[1])
