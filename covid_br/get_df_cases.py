5c6a98669de4f0e5ec6bdf10fe56d4a881ad2046
josedembo <josedembo18@aluno.unilab.edu.br>
2022-05-12 03:57:24 -0300

    Adiciona codigo para criar o csv last_updated_dates

--- covid_br/get_df_cases.py
+++ covid_br/get_df_cases.py
@@ -24,18 +24,38 @@ df_campis = df_cases[df_filter][columns]
 df_campis.columns = ["city", "city_ibge_code", "date", "state", "last_available_confirmed", "last_available_confirmed_per_100k_inhabitants", "last_available_deaths", "new_confirmed", "new_deaths"]
 
 df_campis = df_campis.sort_values(by="city")
+df_campis["city"] = df_campis["city"].apply(rename_city)
 
 df_Acarape = df_campis[df_campis["city"] == "Acarape/CE"].sort_values(by="date", ascending=False)
 df_Redencao = df_campis[df_campis["city"] == "Redenção/CE"].sort_values(by="date", ascending=False)
 df_SFC = df_campis[df_campis["city"] == "São Francisco do Conde/BA"].sort_values(by="date", ascending=False)
 
-
 df_campis = pd.concat([df_Acarape, df_Redencao, df_SFC])
 
+
+last_updated_date = []
+last_updated_date.append(get_last_update_date(df_Acarape))
+last_updated_date.append(get_last_update_date(df_Redencao))
+last_updated_date.append(get_last_update_date(df_SFC))
 
 df_campis.reset_index(inplace=True)
 
 df_campis.drop("index", axis=1, inplace=True)
 
-df_campis.to_csv("data/df_cidades_campi.csv", index=False)
+directory = "./data"
 
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+df_campis.to_csv(f"{directory}/df_cidades_campi.csv", index=False)
+
+with open(f"{directory}/last_updated_dates.csv", "w", encoding="utf-8") as csv_file:
+    csv_writer= csv.writer(csv_file)
+    csv_writer.writerow(["city", "dates"])
+    for row in last_updated_date:
+        city = row["city"]
+        date = row["date"]
+        csv_writer.writerow([
+            city,
+            date
+        ])
