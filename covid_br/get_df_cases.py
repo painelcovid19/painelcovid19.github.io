7255a9b5cd7895fa6e233907918e5a1851ed703f
pedro_dembo <pedro_dembo@atlantico.com.br>
2022-09-14 06:58:09 -0300

    Actualiza o script de coleta dos dados
    -  Agora o script gera dois arquivos csv:
    	* Um para a gerar os graficos dos óbitos e casos confirmados
    	* O outro gerar os graficos de elustração do rt

--- covid_br/get_df_cases.py
+++ covid_br/get_df_cases.py
@@ -3,7 +3,6 @@ import os
 import csv
 from packages.utils import get_last_update_date, rename_city
 
-df_cases = pd.read_csv("https://github.com/wcota/covid19br/blob/master/cases-brazil-cities-time.csv.gz?raw=true", compression='gzip')
 
 columns = [
     "city",
@@ -17,37 +16,46 @@ columns = [
       "newDeaths"
       ]
 
-df_filter = (df_cases["city"] == "Acarape/CE") | (df_cases["city"] == "Redenção/CE") | (df_cases["city"] == "São Francisco do Conde/BA")
+campis_url = "https://github.com/wcota/covid19br/blob/master/cases-brazil-cities-time_changesOnly.csv.gz?raw=true"
+rt_url = "https://github.com/wcota/covid19br/blob/master/cases-brazil-cities-time.csv.gz?raw=true"
 
-df_campis = df_cases[df_filter][columns]
+def get_dataset(url):
 
-df_campis.columns = ["city", "city_ibge_code", "date", "state", "last_available_confirmed", "last_available_confirmed_per_100k_inhabitants", "last_available_deaths", "new_confirmed", "new_deaths"]
+    df_cases = pd.read_csv(url, compression='gzip')
 
-df_campis = df_campis.sort_values(by="city")
-df_campis["city"] = df_campis["city"].apply(rename_city)
+    df_filter = (df_cases["city"] == "Acarape/CE") | (df_cases["city"] == "Redenção/CE") | (df_cases["city"] == "São Francisco do Conde/BA") \
+    | (df_cases["city"] == "Fortaleza/CE") | (df_cases["city"] == "Salvador/BA") 
 
-df_Acarape = df_campis[df_campis["city"] == "Acarape"].sort_values(by="date", ascending=False)
-df_Redencao = df_campis[df_campis["city"] == "Redenção"].sort_values(by="date", ascending=False)
-df_SFC = df_campis[df_campis["city"] == "São Francisco do Conde"].sort_values(by="date", ascending=False)
+    df_cities = df_cases[df_filter][columns]
 
-df_campis = pd.concat([df_Acarape, df_Redencao, df_SFC])
+    df_cities.columns = ["city", "city_ibge_code", "date", "state", "last_available_confirmed", "last_available_confirmed_per_100k_inhabitants", "last_available_deaths", "new_confirmed", "new_deaths"]
 
+    df_cities = df_cities.sort_values(by="city")
+    df_cities["city"] = df_cities["city"].apply(rename_city)
 
-last_updated_date = []
-last_updated_date.append(get_last_update_date(df_Acarape))
-last_updated_date.append(get_last_update_date(df_Redencao))
-last_updated_date.append(get_last_update_date(df_SFC))
+    df_Acarape =df_cities[df_cities["city"] == "Acarape"].sort_values(by="date", ascending=False)
+    df_Redencao = df_cities[df_cities["city"] == "Redenção"].sort_values(by="date", ascending=False)
+    df_SFC = df_cities[df_cities["city"] == "São Francisco do Conde"].sort_values(by="date", ascending=False)
+    df_Fortaleza = df_cities[df_cities["city"] == "Fortaleza"].sort_values(by="date", ascending=False)
+    df_Salvador = df_cities[df_cities["city"] == "Salvador"].sort_values(by="date", ascending=False)
 
-df_campis.reset_index(inplace=True)
+    df_cities = pd.concat([df_Acarape, df_Redencao, df_SFC, df_Fortaleza, df_Salvador])
+    df_cities.reset_index(inplace=True)
+    df_cities.drop("index", axis=1, inplace=True)
 
-df_campis.drop("index", axis=1, inplace=True)
+    return df_cities
 
 directory = "./data"
-
 if not os.path.exists(directory):
     os.makedirs(directory)
 
+df_campis = get_dataset(url=campis_url)
 df_campis.to_csv(f"{directory}/df_cidades_campi.csv", index=False)
+
+last_updated_date = []
+last_updated_date.append(get_last_update_date(df_campis[df_campis["city"] == "Acarape"]))
+last_updated_date.append(get_last_update_date(df_campis[df_campis["city"] == "Redenção"]))
+last_updated_date.append(get_last_update_date(df_campis[df_campis["city"] == "São Francisco do Conde"]))
 
 with open(f"{directory}/last_update_dates.csv", "w", encoding="utf-8") as csv_file:
     csv_writer= csv.writer(csv_file)
@@ -59,3 +67,6 @@ with open(f"{directory}/last_update_dates.csv", "w", encoding="utf-8") as csv_fi
             city,
             date
         ])
+
+df_rt = get_dataset(url=rt_url)
+df_rt.to_csv(f"{directory}/df_cidades_rt.csv", index=False)
